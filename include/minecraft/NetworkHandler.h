6642694ba02c6b01ee1200b7caa73025c0009e56
CodeHz <codehz@outlook.com>
2018-10-26 16:53:35 +0800

    Remove include

--- include/minecraft/NetworkHandler.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-struct Scheduler;
-
-
-struct NetworkHandler {
-  NetworkHandler(Scheduler &);
-};
--- include/mods.h
+++
@@ -1,2 +0,0 @@
-#pragma once
-#define EXPORT extern "C" __attribute__((__visibility__("default")))
